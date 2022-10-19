2c7f28be60380b0c51ab428d0e21c5e99ca77a69
Sergei Poselenov <sergei@voxelbotics.com>
2022-10-19 17:44:24 +0200

    RM-6432. Fixed build script for desktop configuration.

--- scripts/build5-15-5.sh
+++ scripts/build5-15-5.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -x
 
+BUILD_DESKTOP="yes"
+
+if [ x"$BUILD_DESKTOP" = "xyes" ]; then
+    MANIFEST="imx-5.15.5-1.0.0_desktop.xml"
+    DISTRO="imx-desktop-xwayland"
+    SETUP="imx-setup-desktop.sh"
+    BUILDRECIPE="imx-image-desktop"
+else
+    MANIFEST="imx-5.15.5-1.0.0.xml"
+    DISTRO="fsl-imx-xwayland"
+    BUILDRECIPE="imx-image-full"
+    SETUP="imx-setup-release.sh"
+fi
+
+
 # make sure cache writes have w other access
 # umask 0000
 umask 0002
@@ -16,9 +31,7 @@ BUILDDIR=build
 mkdir -p $BUILDDIR
 cd $BUILDDIR
 
-repo init -u https://source.codeaurora.org/external/imx/imx-manifest  -b imx-linux-honister -m imx-5.15.5-1.0.0.xml
-
-#repo init -u https://source.codeaurora.org/external/imx/imx-manifest -b imx-linux-honister -m imx-5.15.5-1.0.0_desktop.xml
+repo init -u https://source.codeaurora.org/external/imx/imx-manifest -b imx-linux-honister -m ${MANIFEST}
 repo sync
 
 # allow build to not prompt for input
@@ -28,8 +41,7 @@ cd sources
 git clone -b imx-5.15.5-vb git@gitlab.com:VoxelBotics/meta-vb-imx8mp.git
 cd ..
 
-DISTRO=fsl-imx-xwayland MACHINE=imx8mpnavq EULA=yes source ./imx-setup-release.sh -b builddir
-
+DISTRO=${DISTRO} MACHINE=imx8mpnavq EULA=yes source ./${SETUP} -b builddir
 
 sed -i 's/^DL_DIR.*$/DL_DIR\ \?=\ \"\/home\/cache\/CACHE\/5.15.5\/downloads\/\"/' conf/local.conf
 echo "SSTATE_DIR = \"/home/cache/CACHE/5.15.5/sstate-cache\"" >> conf/local.conf
@@ -39,6 +51,6 @@ echo BBLAYERS += \"\${BSPDIR}/sources/meta-vb-imx8mp\" >> conf/bblayers.conf
 #devtool modify u-boot-imx
 #devtool modify linux-imx
 
-bitbake imx-image-full
+bitbake ${BUILDRECIPE}
 
 finish=`date +%s`; echo "### Build Time = `expr \( $finish - $start \) / 60` minutes"
