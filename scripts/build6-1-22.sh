4903fcd4c45160be28ee64f752f131ca4e38455e
Sergei Poselenov <sergei@voxelbotics.com>
2023-10-06 13:45:23 +0200

    NAVQP-178: Removed "gstreamer1.0-plugins-bad-curl" plugin form the build, which causes installation of /usr/lib/libcurl.so.4 (dup of /lib/aarch64*/libcurl.so.4), which causes warning when running "cmake" or "curl".

--- scripts/build6-1-22.sh
+++ scripts/build6-1-22.sh
@@ -131,6 +131,11 @@ DISTRO=${DISTRO} MACHINE=imx8mpnavq EULA=yes BUILD_DIR=builddir source ./${SETUP
 sed -i 's/^DL_DIR.*$/DL_DIR\ \?=\ \"\/home\/cache\/CACHE\/6.1.22\/downloads\/\"/' conf/local.conf || exit $?
 echo "SSTATE_DIR = \"/home/cache/CACHE/6.1.22/sstate-cache\"" >> conf/local.conf || exit $?
 echo "BBMASK += \"$BBMASK\"" >> conf/local.conf || exit $?
+
+# Don't build gstreamer curl plugin, to remove dependency of libcurl,
+# to avoid installing duplicate libraries.
+echo "PACKAGECONFIG:remove:pn-gstreamer1.0-plugins-bad = \"curl\"" >> conf/local.conf || exit $?
+
 sed -i -e "s/BB_DEFAULT_UMASK =/BB_DEFAULT_UMASK ?=/" ../sources/poky/meta/conf/bitbake.conf
 sed -i -e "s/PACKAGE_CLASSES ?\?=.*$/PACKAGE_CLASSES ?= \"package_$PACKAGING\"/" conf/local.conf
 
